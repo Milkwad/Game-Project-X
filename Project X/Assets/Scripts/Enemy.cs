17de64ae3c575be484b50a170ed8391eedc9eda3
Gabriel chang <gchang@kth.se>
2018-06-16 15:43:08 +0200

    Add gridnodes, pathfinding ai, navmesh and target

---
+++ Project X/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    public NavMeshAgent enemy;
+
+    
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        //Vector3 end = transform.position + Vector3.left * 10 ;
+        enemy.SetDestination(target.transform.position);
+        
+	}
+
+     void Update()
+    {
+        if (gameObject.transform.position.x == target.transform.position.x)
+            Destroy(this.gameObject);
+
+       
+    }
+
+}
