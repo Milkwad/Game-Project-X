d4ebd81eb3ef26f005d51606daea538903d07e72
Gabriel chang <gchang@kth.se>
2018-06-18 17:23:38 +0200

    add wavespawning and timer for countdown
    added wavespawning and spawning for mobs with a countdown.
    mobs pauses 2 seconds before moving.
    mobs get destroyed when reaching the target.
    fixed navmesh bug by setting the layer to default.

--- Project X/Assets/Scripts/Enemy.cs
+++ Project X/Assets/Scripts/Enemy.cs
@@ -3,25 +3,45 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
-
+    private float wait = 2f;
     public NavMeshAgent enemy;
-
-    
-    public GameObject target;
+    public Transform target;
 
 	// Use this for initialization
 	void Start () {
-        //Vector3 end = transform.position + Vector3.left * 10 ;
-        enemy.SetDestination(target.transform.position);
+        wait = 2f;
+        enemy.SetDestination(target.transform.position + Vector3.left);
+        enemy.isStopped = true;
         
 	}
 
      void Update()
     {
-        if (gameObject.transform.position.x == target.transform.position.x)
-            Destroy(this.gameObject);
+      
+        if (this.gameObject.transform.position.x <= target.transform.position.x)
+        {
 
+            die();
+        }
        
+       
+        if (Mathf.Floor(wait) == 0f)
+        {
+            enemy.isStopped = false; ;
+            wait = -1f;
+        }
+        if(wait > 0f)
+        {
+            wait -= Time.deltaTime;
+        }
+         
+
+    }
+
+    void die()
+    {
+        WaveSpawner.MobsAlive--;
+        Destroy(this.gameObject);
     }
 
 }
---
+++ Project X/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WaveSpawner : MonoBehaviour
+{
+    private float TimeNextWave = 11f;
+    private int WaveRound = 1;
+    public Text waveCountdownText;
+
+    public Transform SpawnPoint;
+    public Transform mobPrefab;
+    public List<Transform> MobList;
+    public int MobsToSpawn = 20;
+
+    public static int MobsAlive = 0;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        SpawnWave();
+    }
+    void Update()
+    {
+        //Debug.Log(MobsAlive);
+
+        if(MobsAlive <= 0)
+        {
+            TimeNextWave -= Time.deltaTime;
+            
+            waveCountdownText.text = Mathf.Floor(TimeNextWave).ToString();
+
+        }
+        if (TimeNextWave <= 1f)
+        {
+            SpawnWave();
+            TimeNextWave = 11f;
+        }
+
+       
+
+
+
+    }
+
+
+
+    void SpawnWave()
+    {
+        MobList = new List<Transform>();
+        for (int i = 0; i < MobsToSpawn; i++)
+        {
+            if (i < MobsToSpawn/2)
+            {
+                Transform mob = (Transform)Instantiate(mobPrefab, SpawnPoint.position + new Vector3(1, 1, i), SpawnPoint.rotation);
+                mob.transform.parent = transform;
+                MobList.Add(mob);
+            }
+            else
+            {
+                Transform mob = (Transform)Instantiate(mobPrefab, SpawnPoint.position + new Vector3(-1.0f, 1, i-10), SpawnPoint.rotation);
+                mob.transform.parent = transform;
+                MobList.Add(mob);
+               
+            }
+           
+        }
+        MobsAlive = MobList.Count;
+        WaveRound++;
+    }
+}
